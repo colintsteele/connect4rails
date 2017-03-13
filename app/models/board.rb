b42dddbdc04d41bbb4d00d676179e4e3101d0b63
Colin Steele <colin.steele@manheim.com>
2017-03-13 14:42:31 -0400

    add board print

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,7 +2,7 @@ require 'singleton'
 
 class Board
   include Singleton
-  attr_reader :height, :width, :grid
+  attr_reader :height, :width, :grid,
 
   def initialize(height = 7, width = 7)
     @height = height
@@ -18,6 +18,19 @@ class Board
     end
   end
 
-
+  def print_board
+    print "\n"
+    through_grid do |column, row|
+      case @grid[row][column]
+        when 1
+          print("[X]")
+        when 2
+          print("[O]")
+        else
+          print("[-]")
+      end
+      print "\n" if row == width - 1
+    end
+  end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -11,8 +11,32 @@ describe 'initial board state' do
   end
 
   it 'should have a 7 x 7 grid' do
-    expect(@board.grid.size).to equal(7)
-    expect(@board.grid.first.size).to equal(7)
+    expect(@board.grid.size).to eq(7)
+    expect(@board.grid.first.size).to eq(7)
+  end
+
+  it 'should be able to traverse the grid' do
+    count = 0
+    @board.through_grid do
+      count += 1
+    end
+    expect(count).to eq(49)
+  end
+
+  it 'should be an entirely empty grid' do
+    not_nil = false
+    @board.through_grid do |c, r|
+      not_nil = true unless @board.grid[c][r].nil?
+    end
+    expect(not_nil).to be_falsey
+  end
+
+end
+
+describe 'dropping discs' do
+
+  before :all do
+    @board = Board.instance
   end
 
 end
